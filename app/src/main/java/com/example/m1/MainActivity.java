115d11f808a2940ece263e259ace190031ec8a5d
IIITSnehil <snehil19046@iiitd.ac.in>
2019-08-13 15:26:44 +0530

    new activity created

--- app/src/main/java/com/example/m1/MainActivity.java
+++ app/src/main/java/com/example/m1/MainActivity.java
@@ -8,11 +8,13 @@ import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String MyTag ="Myactivity";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(MyTag,"In on Create");
     }
 
 
@@ -22,5 +24,33 @@ public class MainActivity extends AppCompatActivity {
             Intent newIntent = new Intent(this,Main2Activity.class);
              startActivity(newIntent);
         }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(MyTag,"In on Start");
+    }
 
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(MyTag,"In on Resume");
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(MyTag,"In on Pause");
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(MyTag,"In on Stop");
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d(MyTag,"In on Destroy");
+        super.onDestroy();
+    }
 }
